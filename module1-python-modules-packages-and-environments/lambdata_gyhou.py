fa55b2e0ad731e9c870b0ccaa041a82fb69da8fb
gyhou <47550192+gyhou@users.noreply.github.com>
2019-08-05 21:44:12 -0500

    Add files via upload

---
+++ module1-python-modules-packages-and-environments/lambdata_gyhou.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def check_nulls(df_nulls):
+    return df_nulls.isnull().sum()
+
+def split_date_columns(X, column_name):
+    X[column_name] = pd.to_datetime(X[column_name])
+    X[column_name+'_year'] = X[column_name].dt.year
+    X[column_name+'_month'] = X[column_name].dt.month
+    X[column_name+'_day'] = X[column_name].dt.day
+    X[column_name] = X[column_name].dt.strftime('%Y-%m-%d')
+    return X
