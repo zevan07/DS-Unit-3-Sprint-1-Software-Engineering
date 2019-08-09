1cf8c700fefe8b75c95d6ff0f11c45cf6990237c
gyhou <47550192+gyhou@users.noreply.github.com>
2019-08-09 13:36:52 -0500

    Add files via upload

--- sprint/acme.py
+++ sprint/acme.py
@@ -14,7 +14,7 @@ class Product(object):
         ratio = self.price / self.weight
         if ratio < .5:
             return 'Not so stealable...'
-        elif 1 > ratio >= .5:
+        elif ratio < 1:
             return 'Kinda stealable.'
         else:
             return 'Very stealable!'
@@ -23,22 +23,21 @@ class Product(object):
         product = self.flammability * self.weight
         if product < 10:
             return '...fizzle.'
-        elif 50 > product >= 10:
+        elif product < 50:
             return '...boom!'
         else:
             return '...BABOOM!!'
 
+    def __str__(self):
+        return ('%s\nPrice: %s\nWeight: %s\nFlammability: %s\nIdentifier: %s' %
+                (self.name, self.price, self.weight,
+                 self.flammability, self.identifier))
+
 
 class BoxingGlove(Product):
-    # def __init__(self, name, price, weight=10, flammability, identifier):
-    #     super().__init__(name, price, weight, flammability, identifier)
-    def __init__(self, name, price=10, weight=10, flammability=.5,
-                 identifier=randint(1000000, 9999999)):
-        self.name = str(name)
-        self.price = int(price)
-        self.weight = int(weight)
-        self.flammability = float(flammability)
-        self.identifier = int(identifier)
+    def __init__(self, name, price=10, weight=10, flammability=.5):
+        Product.__init__(self, name, price, weight, flammability)
+        self.weight = int(10)
 
     def explode(self):
         return "...it's a glove."
@@ -46,7 +45,7 @@ class BoxingGlove(Product):
     def punch(self):
         if self.weight < 5:
             return 'That tickles.'
-        elif 15 > self.weight >= 5:
+        elif self.weight < 15:
             return 'Hey that hurt!'
         else:
             return 'OUCH!'
--- sprint/acme_test.py
+++ sprint/acme_test.py
@@ -9,16 +9,38 @@ class AcmeProductTests(unittest.TestCase):
         """Test default product price being 10."""
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
-        self.assertEqual(prod.stealability(), 'Kinda stealable.')
-        self.assertEqual(prod.explode(), '...boom!')
-​
-​class AcmeReportTests(unittest.TestCase):
+
+    def test_stealability(self):
+        p1 = Product('prod1')
+        p2 = Product('prod2', 5, 20)
+        p3 = Product('prod3', 20, 20)
+        p4 = Product('prod4', 25, 20)
+
+        self.assertEqual(p1.stealability(), "Kinda stealable.")
+        self.assertEqual(p2.stealability(), "Not so stealable...")
+        self.assertEqual(p3.stealability(), "Very stealable!")
+        self.assertEqual(p4.stealability(), "Very stealable!")
+
+    def test_explode(self):
+        p1 = Product('prod1', 1, 19, 0.5)
+        p2 = Product('prod2', 1, 21, 0.5)
+        p3 = Product('prod3', 1, 48, 1)
+        p4 = Product('prod4', 1, 50, 2)
+
+        self.assertEqual(p1.explode(), "...fizzle.")
+        self.assertEqual(p2.explode(), "...boom!")
+        self.assertEqual(p3.explode(), "...boom!")
+        self.assertEqual(p4.explode(), "...BABOOM!!")
+
+
+class AcmeReportTests(unittest.TestCase):
     def test_default_num_products(self):
         self.assertEqual(len(generate_products()), 30)
 
     def test_legal_names(self):
         for product in generate_products():
             name = product.name
+            self.assertEqual(name.count(' '), 1)
             adj, noun = name.split(' ')
             self.assertIn(adj, ADJECTIVES)
             self.assertIn(noun, NOUNS)
