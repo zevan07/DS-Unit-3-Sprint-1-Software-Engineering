115b47d8275d46fd20dba92aa70c570f1664490a
zevan07 <50527755+zevan07@users.noreply.github.com>
2019-08-14 09:36:20 -0600

    Add docstrings and change to PEP8 formatting

--- module1-python-modules-packages-and-environments/lambdata_gyhou.py
+++ module1-python-modules-packages-and-environments/lambdata_gyhou.py
@@ -1,9 +1,26 @@
 import pandas as pd
 
+
 def check_nulls(df_nulls):
+    '''
+    Return number of nulls in a dataframe.
+
+    df_nulls -- the dataframe to be checked for null values
+    '''
     return df_nulls.isnull().sum()
 
+
 def split_date_columns(X, column_name):
+    '''
+    Return columns with year, month, and day.
+    Converts original column back to string from datetime.
+
+    Keyword arguments:
+    X -- the dataframe to be adjusted with new date columns
+    column_name -- the name of the column that is to be used to infer datetime,
+                   will also be used in naming of new columns
+    '''
+
     X[column_name] = pd.to_datetime(X[column_name])
     X[column_name+'_year'] = X[column_name].dt.year
     X[column_name+'_month'] = X[column_name].dt.month
