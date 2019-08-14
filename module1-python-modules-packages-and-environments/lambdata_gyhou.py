2631a382ceef4017a379157a991c7b2fc1cb1949
zevan07 <50527755+zevan07@users.noreply.github.com>
2019-08-14 09:19:52 -0600

    Update lambdata_gyhou.py

--- module1-python-modules-packages-and-environments/lambdata_gyhou.py
+++ module1-python-modules-packages-and-environments/lambdata_gyhou.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+
 def check_nulls(df_nulls):
     return df_nulls.isnull().sum()
+
 
 def split_date_columns(X, column_name):
     X[column_name] = pd.to_datetime(X[column_name])
